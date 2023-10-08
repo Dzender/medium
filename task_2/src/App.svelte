145cf3992be56adc0f73f2223c31c5d91235e885
Zohan <61355856+Dzender@users.noreply.github.com>
2023-10-08 09:56:30 +0300

    Add files via upload

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,56 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { fetchApiData } from './lib/counter';
+  import Radiobuttons from './lib/radiobuttons.svelte';
+
+
+  function handleClick() {
+    const data = fetchApiData('https://v6.exchangerate-api.com/v6/e0e5ee25c36472e2dd5773aa/latest/USD')
+    console.log(data); 
+  }
+
+  let isRadioButtonsVisible: boolean = true;
+  function toggleRadioButtonsVisibility() {
+    isRadioButtonsVisible = !isRadioButtonsVisible;
+  }
+
+  function firstInputChange(){
+    
+  }
+
+  function secondInputChange(){
+
+  }
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="wrapper">
+    <div class="currencySelect">
+      {#if isRadioButtonsVisible}
+        <Radiobuttons/>
+      {/if}
+      <button class="seeMore" on:click={toggleRadioButtonsVisibility}>
+          {#if isRadioButtonsVisible}
+            Свернуть выбор
+          {:else}
+            Развернуть выбор
+          {/if}
+      </button>
+    </div>
+    <div class="currencyAmount">
+      <input type="number" class="firstInput" on:change={firstInputChange}/>
+      <input type="number" class="secondInput" on:change={secondInputChange}/>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .seeMore{
+    height: 100%;
+    width: 100%;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .wrapper{
+    height: auto;
+    width: 100%;
   }
 </style>
