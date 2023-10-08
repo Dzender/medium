d17bcbfc7975c6b7171c5610e78b1b1401ba8cd4
Zohan <61355856+Dzender@users.noreply.github.com>
2023-10-08 09:54:56 +0300

    Add files via upload

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,34 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decodeFields(data) {
+    const decodedData = data.map((item) => {
+      const decodedItem = { ...item };
+      for (const key in item) {
+        if (key.endsWith('Id') && key !== 'groupId' && key !== 'service' && key !== 'formatSize' && key !== 'ca') {
+          const id = item[key];
+          decodedItem[key] = translations[id] || id;
+        }
+      }
+      return decodedItem;
+    });
+  
+    const uniqueIds = new Set();
+    data.forEach((item) => {
+      for (const key in item) {
+        if (key.endsWith('Id')) {
+          uniqueIds.add(item[key]);
+        }
+      }
+    });
+  
+    return { decodedData, uniqueIds };
+}
 
+const { decodedData, uniqueIds } = decodeFields(encoded);
 
+console.log("Let's rock");
+console.log(encoded, translations);
 
-// console.log(decoded)
+console.log(decodedData);
+console.log(uniqueIds);
+
